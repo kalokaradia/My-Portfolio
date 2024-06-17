ad1e84321f360dddf5037790790a8a379e41ad0d
kalokaradia <kalokaradia@gmail.com>
2024-06-17 21:08:20 +0700

    menambahkan icon ke web

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <link rel="icon" href="/logo.ico" type="image/x-icon" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
