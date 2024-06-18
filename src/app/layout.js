6fcb970d411a68b63a23b6c9aa213a5fca7cc356
kalokaradia <kalokaradia@gmail.com>
2024-06-18 20:39:56 +0700

    mengedit beberapa kode di file layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,4 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Kaloka Radia Nanda",
@@ -20,7 +17,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
