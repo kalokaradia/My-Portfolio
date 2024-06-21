432ffbb653a5297f6073f158b19a1c6e098bd4e5
kalokaradia <kalokaradia@gmail.com>
2024-06-21 19:17:02 +0700

    mengedit typo di submitEmail.js dan mengisi file .evn.example

--- src/lib/submitEmail.js
+++ src/lib/submitEmail.js
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 
 export default async function submitEmail({ email, name, message }) {
   console.log(`email: ${email}, name: ${name}, message: ${message}`);
-  const resend = new Resend(process.env.RESEN_API_KEY);
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
   const result = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
